b10297b8944a8a1c8bb7caccb856e24d038aea1c
Kilian Eisenegger <kilian.eisenegger@bluewin.ch>
2019-01-10 20:02:05 +0100

    1.0.2

---
+++ Hdrpano.framework/Headers/Hdrpano.h
@@ -0,0 +1,19 @@
+//
+//  Hdrpano.h
+//  Hdrpano
+//
+//  Created by Kilian Eisenegger on 29.12.18.
+//  Copyright © 2018 Kilian Eisenegger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Hdrpano.
+FOUNDATION_EXPORT double HdrpanoVersionNumber;
+
+//! Project version string for Hdrpano.
+FOUNDATION_EXPORT const unsigned char HdrpanoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hdrpano/PublicHeader.h>
+
+
